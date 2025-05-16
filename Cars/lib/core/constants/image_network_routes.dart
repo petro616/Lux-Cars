0e917c1a115f3f5f50f05222e5b7a9f29d3483c9
Rebal Daher <rebaldaher29@gmail.com>
2025-05-16 10:48:24 -0700

    Delete Cars directory

--- Cars/lib/core/constants/image_network_routes.dart
+++
@@ -1,6 +0,0 @@
-class ImageNetworkRoutes {
-  static const String imageBaseCategories =
-      "http://192.168.43.4/cars_backend/upload/categories/";
-  static const String imageBaseCars =
-      "http://192.168.43.4/cars_backend/upload/items/";
-}
--- Cars/lib/core/constants/named_routes.dart
+++
@@ -1,7 +0,0 @@
-class Routes {
-  static const String onboarding = "/";
-  static const String home = "/home";
-  static const String carDetails = "/carDetails";
-  static const String categories = "/categories";
-  static const String privacyAndPolicy = "/privacyAndPolicy";
-}
